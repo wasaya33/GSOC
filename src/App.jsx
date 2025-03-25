604b41b6002bf9a07fce325fa7246bfabd7f0a80
Allah wasaya <awasaya058@gmail.com>
2025-03-25 21:55:42 +0500

    start project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+import HomePage from './Pages/HomePage/HomePage'
+
+function App() {
+
+  return (
+    <>
+     
+      <HomePage />
+    </>
+  )
+}
+
+export default App
